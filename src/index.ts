8ceb56420974de0532a3ca3b3387dfcbb5057fe5
Duc Nguyen <minhduc140583@gmail.com>
2026-04-12 16:17:54 +0700

    Add transaction for SqlRateRepository, remove SqlRatesRepository

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,89 @@
-import { Attributes, buildMetadata, buildToInsert, buildToUpdate, DB, Statement, StringMap } from 'query-core';
-import { RateRepository } from './core-query';
+export type DataType =
+  | "ObjectId"
+  | "date"
+  | "datetime"
+  | "time"
+  | "boolean"
+  | "number"
+  | "integer"
+  | "string"
+  | "text"
+  | "object"
+  | "array"
+  | "binary"
+  | "primitives"
+  | "booleans"
+  | "numbers"
+  | "integers"
+  | "strings"
+  | "dates"
+  | "datetimes"
+  | "times"
+export type Operator = "=" | "like" | "!=" | "<>" | ">" | ">=" | "<" | "<="
 
-export * from './core-query';
+export interface Attribute {
+  name?: string
+  column?: string
+  type?: DataType
+  default?: string | number | Date | boolean
+  key?: boolean
+  q?: boolean
+  noinsert?: boolean
+  noupdate?: boolean
+  nopatch?: boolean
+  version?: boolean
+  ignored?: boolean
+  true?: string | number
+  false?: string | number
+  createdAt?: boolean
+  updatedAt?: boolean
+}
+export interface Attributes {
+  [key: string]: Attribute
+}
 
-export class SqlRateRepository<R> implements RateRepository<R> {
-  constructor(protected db: DB, protected table: string, protected attributes: Attributes, protected buildToSave: <K>(obj: K, table: string, attrs: Attributes, ver?: string, buildParam?: (i: number) => string, i?: number) => Statement | undefined, protected max: number, protected infoTable: string, rateField?: string, count?: string, score?: string, authorCol?: string, id?: string, idField?: string, idCol?: string, rateCol?: string) {
-    const m = buildMetadata(attributes);
-    this.map = m.map;
+export interface Executor {
+  driver: string
+  param(i: number): string
+  execute(sql: string, args?: any[], ctx?: any): Promise<number>
+  executeBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>
+  query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>
+}
+export interface Transaction extends Executor {
+  commit(): Promise<void>
+  rollback(): Promise<void>
+}
+export interface DB extends Executor {
+  beginTransaction(): Promise<Transaction>
+}
+export interface StringMap {
+  [key: string]: string
+}
+export interface Statement {
+  query: string
+  params?: any[]
+}
+
+export function buildMap(attrs: Attributes): StringMap {
+  const mp: StringMap = {}
+  const ks = Object.keys(attrs)
+  for (const k of ks) {
+    const attr = attrs[k]
+    attr.name = k
+    const field = attr.column ? attr.column : k
+    const s = field.toLowerCase()
+    if (s !== k) {
+      mp[s] = k
+    }
+  }
+  return mp
+}
+export class SqlRateRepository<R> {
+  constructor(protected db: DB, protected table: string, protected attributes: Attributes, protected max: number, protected infoTable: string,
+    protected buildToInsert: (obj: R, table: string, attrs: Attributes, buildParam: (i: number) => string) => Statement,
+    protected buildToUpdate: (obj: R, table: string, attrs: Attributes, buildParam: (i: number) => string) => Statement,
+    protected generateId: () => string, protected rateIdField: string, rateField?: string, count?: string, score?: string, authorCol?: string, id?: string, idField?: string, idCol?: string, rateCol?: string) {
+    this.map = buildMap(attributes);
     this.id = (id && id.length > 0 ? id : 'id');
     this.rate = (rateCol && rateCol.length > 0 ? rateCol : 'rate');
     this.count = (count && count.length > 0 ? count : 'count');
@@ -50,25 +127,28 @@ export class SqlRateRepository<R> implements RateRepository<R> {
   rateField: string;
   idCol: string;
   authorCol: string;
-  load(id: string, author: string, ctx?: any): Promise<R | null> {
-    return this.db.query<R>(`select * from ${this.table} where ${this.idCol} = ${this.db.param(1)} and ${this.authorCol} = ${this.db.param(2)}`, [id, author], this.map, undefined, ctx).then(rates => {
+  load(id: string, author: string, tx?: Transaction): Promise<R | null> {
+    const db = tx ? tx : this.db
+    return db.query<R>(`select * from ${this.table} where ${this.idCol} = ${this.db.param(1)} and ${this.authorCol} = ${this.db.param(2)}`, [id, author], this.map).then(rates => {
       return rates && rates.length > 0 ? rates[0] : null;
     });
   }
-  create(rate: R, newInfo?: boolean): Promise<number> {
-    const stmt = buildToInsert(rate, this.table, this.attributes, this.db.param);
-    if (stmt) {
+  create(rate: R, newInfo?: boolean, tx?: Transaction): Promise<number> {
+    (rate as any)[this.rateIdField] = this.generateId()
+    const stmt = this.buildToInsert(rate, this.table, this.attributes, this.db.param);
+    if (stmt.query) {
       const obj: any = rate;
       const rateNum: number = obj[this.rateField];
       const id: string = obj[this.idField];
+      const db = tx ? tx : this.db
       if (newInfo) {
         const query = this.insertInfo(rateNum);
         const s2: Statement = { query, params: [id] };
-        return this.db.executeBatch([s2, stmt], true);
+        return db.executeBatch([s2, stmt], true);
       } else {
         const query = this.updateNewInfo(rateNum);
         const s2: Statement = { query, params: [id] };
-        return this.db.executeBatch([s2, stmt], true);
+        return db.executeBatch([s2, stmt], true);
       }
     } else {
       return Promise.resolve(-1);
@@ -90,15 +170,16 @@ export class SqlRateRepository<R> implements RateRepository<R> {
       values (${this.db.param(1)}, ${r}, 1, ${r}, ${ps.join(',')})`;
     return query;
   }
-  update(rate: R, oldRate: number): Promise<number> {
-    const stmt = buildToUpdate(rate, this.table, this.attributes, this.db.param);
-    if (stmt) {
+  update(rate: R, oldRate: number, tx?: Transaction): Promise<number> {
+    const stmt = this.buildToUpdate(rate, this.table, this.attributes, this.db.param);
+    if (stmt.query) {
       const obj: any = rate;
       const rateNum: number = obj[this.rateField];
       const id: string = obj[this.idField];
       const query = this.updateOldInfo(rateNum, oldRate);
       const s2: Statement = { query, params: [id] };
-      return this.db.executeBatch([s2, stmt], true);
+      const db = tx ? tx : this.db
+      return db.executeBatch([s2, stmt], true);
     } else {
       return Promise.resolve(-1);
     }
@@ -115,207 +196,8 @@ export class SqlRateRepository<R> implements RateRepository<R> {
     }
     const delta = newRate - oldRate;
     const query = `
-      update ${this.infoTable} set ${this.rate} = (${this.score} + ${delta})/${this.count}, ${this.score} = ${this.score} + ${delta}, ${this.rate}${newRate} = ${this.rate}${newRate} + 1, ${this.rate}${oldRate} = ${this.rate}${oldRate} - 1
+      update ${this.infoTable} set ${this.rate} = (${this.score} + (${delta}))/${this.count}, ${this.score} = ${this.score} + (${delta}), ${this.rate}${newRate} = ${this.rate}${newRate} + 1, ${this.rate}${oldRate} = ${this.rate}${oldRate} - 1
       where ${this.id} = ${this.db.param(1)}`;
     return query;
-  }
-}
-export interface BaseRate {
-  rate: number;
-  rates: number[];
-}
-export function avg(n: number[]): number {
-  let sum = 0;
-  for (const s of n) {
-    sum = sum + s;
-  }
-  return sum / n.length;
-}
-export class SqlRatesRepository<R extends BaseRate> {
-  constructor(protected db: DB, protected table: string, protected fullTable: string, protected tables: string[], protected attributes: Attributes,
-    protected buildToSave: <K>(obj: K, table: string, attrs: Attributes, ver?: string, buildParam?: (i: number) => string, i?: number) => Statement | undefined,
-    protected max: number, rateField?: string, count?: string, score?: string, authorCol?: string, id?: string, idField?: string, idCol?: string,
-    rateCol?: string) {
-    const m = buildMetadata(attributes);
-    this.map = m.map;
-    this.id = (id && id.length > 0 ? id : 'id');
-    this.rate = (rateCol && rateCol.length > 0 ? rateCol : 'rate');
-    this.count = (count && count.length > 0 ? count : 'count');
-    this.score = (score && score.length > 0 ? score : 'score');
-    this.idField = (idField && idField.length > 0 ? idField : 'id');
-    this.rateField = (rateField && rateField.length > 0 ? rateField : 'rate');
-    this.authorCol = (authorCol && authorCol.length > 0 ? authorCol : 'author');
-    if (idCol && idCol.length > 0) {
-      this.idCol = idCol;
-    } else {
-      const c = attributes[this.idField];
-      if (c) {
-        this.idCol = (c.column && c.column.length > 0 ? c.column : this.idField);
-      } else {
-        this.idCol = this.idField;
-      }
-    }
-    if (rateCol && rateCol.length > 0) {
-      this.rate = rateCol;
-    } else {
-      const c = attributes[this.rateField];
-      if (c) {
-        this.rate = (c.column && c.column.length > 0 ? c.column : this.rateField);
-      } else {
-        this.rate = this.rateField;
-      }
-    }
-    this.load = this.load.bind(this);
-    this.create = this.create.bind(this);
-    this.update = this.update.bind(this);
-    this.insertInfo = this.insertInfo.bind(this);
-    this.insertFullInfo = this.insertFullInfo.bind(this);
-    this.updateFullInfo = this.updateFullInfo.bind(this);
-    this.updateNewInfo = this.updateNewInfo.bind(this);
-    this.updateOldInfo = this.updateOldInfo.bind(this);
-  }
-  map?: StringMap;
-  count: string;
-  score: string;
-  id: string;
-  rate: string;
-  idField: string;
-  rateField: string;
-  idCol: string;
-  authorCol: string;
-
-  load(id: string, author: string, ctx?: any): Promise<R | null> {
-    return this.db.query<R>(`select * from ${this.table} where ${this.idCol} = ${this.db.param(1)} and ${this.authorCol} = ${this.db.param(2)}`, [id, author], this.map, undefined, ctx).then(rates => {
-      return rates && rates.length > 0 ? rates[0] : null;
-    });
-  }
-  create(rate: R, newInfo?: boolean): Promise<number> {
-    if (rate.rates.length !== this.tables.length) {
-      return Promise.reject('Invalid rates length');
-    }
-    const obj: any = rate;
-    const id: string = obj[this.idField];
-    const mainStmt = buildToInsert<R>(rate, this.table, this.attributes, this.db.param);
-    if (!mainStmt) {
-      return Promise.reject('cannot build to insert rate');
-    }
-    const stmts: Statement[] = [];
-    if (newInfo) {
-      for (let i = 0; i < rate.rates.length; i++) {
-        const sql = this.insertInfo(rate.rates[i], this.tables[i]);
-        stmts.push({ query: sql, params: [id] });
-      }
-      const fullStmt: Statement = { query: this.insertFullInfo(rate.rate, this.fullTable, this.tables), params: [id, id, id, id, id, id] };
-      stmts.push(fullStmt);
-      stmts.push(mainStmt);
-      return this.db.executeBatch(stmts, true);
-    } else {
-      const fullStmt: Statement = { query: this.updateFullInfo(rate.rate, this.fullTable, this.tables), params: [id, id, id, id, id, id] };
-      stmts.push(fullStmt);
-      for (let i = 0; i < rate.rates.length; i++) {
-        const sql = this.updateNewInfo(rate.rates[i], this.tables[i]);
-        stmts.push({ query: sql, params: [id] });
-      }
-      stmts.push(mainStmt);
-      return this.db.executeBatch(stmts, true);
-    }
-  }
-  protected insertInfo(r: number, table: string): string {
-    const rateCols: string[] = [];
-    const ps: string[] = [];
-    for (let i = 1; i <= this.max; i++) {
-      rateCols.push(`${this.rate}${i}`);
-      if (i === r) {
-        ps.push('' + 1);
-      } else {
-        ps.push('0');
-      }
-    }
-    const query = `
-      insert into ${table} (${this.id}, ${this.rate}, ${this.count}, ${this.score}, ${rateCols.join(',')})
-      values (${this.db.param(1)}, ${r}, 1, ${r}, ${ps.join(',')})`;
-    return query;
-  }
-  protected insertFullInfo(r: number, table: string, tables: string[]): string {
-    const rateCols: string[] = [];
-    const s: string[] = [];
-    for (let i = 1; i <= tables.length; i++) {
-      rateCols.push(`${this.rate}${i}`);
-      s.push(`(select avg(${this.rate}) from ${tables[i - 1]} where ${this.id} = ${this.db.param(i)} group by ${this.id})`);
-    }
-    const query = `
-      insert into ${table} (${this.id}, ${this.rate}, ${this.count}, ${this.score}, ${rateCols.join(', ')})
-      values (${this.db.param(6)}, ${r}, 1, ${r}, ${s.join(',')})`;
-    return query;
-  }
-  protected updateFullInfo(r: number, table: string, tables?: string[]): string {
-    if (tables && tables.length > 0) {
-      const s: string[] = [];
-      for (let i = 1; i <= tables.length; i++) {
-        s.push(`${this.rate}${i} = (select avg(${this.rate}) from ${tables[i - 1]} where ${this.id} = ${this.db.param(i)} group by ${this.id})`);
-      }
-      const query = `
-        update ${table} set ${this.rate} = (${this.score} + ${r})/(${this.count} + 1), ${this.score} = ${this.score} + ${r},${this.count} = ${this.count} + 1, ${s.join(',')}
-        where ${this.id} = ${this.db.param(6)}`;
-      return query;
-    } else {
-      const query = `
-        update ${table} set ${this.rate} = (${this.score} + ${r})/(${this.count} + 1), ${this.score} = ${this.score} + ${r}, ${this.count} = ${this.count} + 1
-        where ${this.id} = ${this.db.param(6)}`;
-      return query;
-    }
-  }
-  protected updateNewInfo(r: number, table: string): string {
-    const query = `
-      update ${table} set ${this.rate} = (${this.score} + ${r})/(${this.count} + 1), ${this.count} = ${this.count} + 1, ${this.score} = ${this.score} + ${r}, ${this.rate}${r} = ${this.rate}${r} + 1
-      where ${this.id} = ${this.db.param(1)}`;
-    return query;
-  }
-  protected updateOldInfo(newRate: number, oldRate: number, table: string, tables?: string[]): string {
-    const delta = newRate - oldRate;
-    const s: string[] = [];
-    if (tables && tables.length > 0) {
-      for (let i = 1; i <= tables.length; i++) {
-        s.push(`${this.rate}${i} = (select avg(${this.rate}) from ${tables[i - 1]} where ${this.id} = ${this.db.param(i)} group by ${this.id})`);
-      }
-      const query = `
-        update ${table} set ${this.rate} = (${this.score} + ${delta})/${this.count}, ${this.score} = ${this.score} + ${delta}, ${this.count} = ${this.count}, ${s.join(',')}
-        where ${this.id} = ${this.db.param(6)}`;
-      return query;
-    } else {
-      const query = `
-        update ${table} set ${this.rate} = (${this.score} + ${delta})/${this.count}, ${this.score} = ${this.score} + ${delta}, ${this.count} = ${this.count}
-        where ${this.id} = ${this.db.param(6)}`;
-      return query;
-    }
-  }
-  update(rate: R, oldRate: number): Promise<number> {
-    const rates = rate.rates;
-    const r = rate.rate;
-    const stmts: Statement[] = [];
-    const stmt = buildToUpdate(rate, this.table, this.attributes, this.db.param);
-    if (r && rates && rates.length > 0) {
-      if (stmt) {
-        const obj: any = rate;
-        const id: string = obj[this.idField];
-        for (let i = 0; i < rate.rates.length; i++) {
-          const sql = this.updateNewInfo(rate.rates[i], this.tables[i]);
-          stmts.push({ query: sql, params: [id] });
-        }
-        const query: Statement = { query: this.updateOldInfo(rate.rate, oldRate, this.fullTable, this.tables), params: [id, id, id, id, id, id] };
-        stmts.push(query);
-        stmts.push(stmt);
-        return this.db.executeBatch(stmts, true);
-      } else {
-        return Promise.resolve(-1);
-      }
-    } else {
-      if (!stmt) {
-        return Promise.reject('cannot build to insert rate');
-      } else {
-        stmts.push(stmt);
-        return this.db.executeBatch(stmts, true);
-      }
-    }
   }
 }
